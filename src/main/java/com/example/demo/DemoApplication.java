f2a66d887453c1ad5ab2cd508fbad7ee1342e38f
krish3402 <40502996+krish3402@users.noreply.github.com>
2020-06-05 13:57:32 +0530

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -10,7 +10,7 @@ public class DemoApplication {
 
 	@GetMapping("/")
 	String home() {
-		return "Hey  Succesful !!!";
+		return "Hey L  Succesful !!!";
 	}g
 
 	public static void main(String[] args) {
