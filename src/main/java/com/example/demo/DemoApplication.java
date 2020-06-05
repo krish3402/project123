7fc387e80d7bb7059cf26e31fbeb9eb05dc38291
krish3402 <40502996+krish3402@users.noreply.github.com>
2020-06-05 18:14:45 +0530

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -11,7 +11,7 @@ public class DemoApplication {
 	@GetMapping("/")
 	String home() {
 		return "Hey Login Succesful !!!";
-	}g
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
